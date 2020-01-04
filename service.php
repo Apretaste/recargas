537f22a24e91ac99369b73867fc9e06e6dc891ee
kumahacker <kumahavana@gmail.com>
2020-01-03 22:42:34 -0500

    migrate

--- service.php
+++ service.php
@@ -1,28 +1,36 @@
 <?php
 
+use Framework\Database;
+use Apretaste\Request;
+use Apretaste\Response;
+
+
 class Service
 {
 	/**
 	 * Main
 	 *
-	 * @author salvipascual
-	 * @param Request $request
+	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @throws \Exception
+	 * @author salvipascual
 	 */
-	public function _main (Request $request, Response $response)
+	public function _main (Request $request, Response &$response)
 	{
 		// check if the user has a cellphone
 		$phone = $request->person->cellphone;
 		if(strlen($phone) != 10 || !substr($phone,0,2) === "53") {
-			return $response->setTemplate("phone.ejs", ["phone"=>$phone]);
+			$response->setTemplate("phone.ejs", ["phone"=>$phone]);
+			return;
 		}
 
 		// get the price for the recharge for today
-		$product = Connection::query("SELECT name, price FROM inventory WHERE code = 'CUBACEL_10'");
+		$product = Database::query("SELECT name, price FROM inventory WHERE code = 'CUBACEL_10'");
 		$price = $product[0]->price;
 
 		// get the recharge for today, or false
-		$recharge = Connection::query("
+		$recharge = Database::query("
 			SELECT A.inserted, B.username, B.avatar, B.avatarColor
 			FROM _recargas A
 			JOIN person B 
@@ -31,7 +39,7 @@ class Service
 		$recharge = empty($recharge) ? false : $recharge[0];
 
 		// check if the user has bough a recharge the current month
-		$lastMonth = !empty(Connection::query("
+		$lastMonth = !empty(Database::query("
 			SELECT id FROM _recargas 
 			WHERE person_id = '{$request->person->id}'
 			AND MONTH(inserted) = MONTH(CURRENT_DATE) AND YEAR(inserted) = YEAR(CURRENT_DATE)"));
@@ -43,7 +51,7 @@ class Service
 		}
 
 		// check if the phone is blocked
-		$phoneIsBlocked = !empty(Connection::query("SELECT * FROM blocked_numbers WHERE cellphone='$phone'"));
+		$phoneIsBlocked = !empty(Database::query("SELECT * FROM blocked_numbers WHERE cellphone='$phone'"));
 
 		// check if the user is a month old
 		$isOldUser = date_diff(new DateTime(), new DateTime($request->person->insertion_date))->days > 60;
@@ -65,14 +73,16 @@ class Service
 	/**
 	 * Check the last 20 previous recharges
 	 *
-	 * @author salvipascual
-	 * @param Request $request
+	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @throws \Framework\Alert
+	 * @author salvipascual
 	 */
-	public function _anteriores (Request $request, Response $response)
+	public function _anteriores (Request $request, Response &$response)
 	{
 		// show a list of previous recharges
-		$recharges = Connection::query("
+		$recharges = Database::query("
 			SELECT B.username, DATE_FORMAT(A.inserted, '%e/%c/%Y %r') AS inserted, B.avatar, B.avatarColor
 			FROM _recargas A
 			JOIN person B 
@@ -89,41 +99,44 @@ class Service
 	/**
 	 * Pay for an item and add the items to the database
 	 *
-	 * @param Request
-	 * @param Response
-	 * @throws Exception
+	 * @param \Apretaste\Request  $request
+	 * @param \Apretaste\Response $response
+	 *
+	 * @throws \Framework\Alert
 	 */
-	public function _pay (Request $request, Response $response)
+	public function _pay (Request $request, Response &$response)
 	{
 		// get buyer and code
 		$buyer = $request->person;
 		$code = $request->input->data->code;
 
 		// check if a recharge was already done today
-		$isMaxReached = Connection::query("SELECT COUNT(id) AS cnt FROM _recargas WHERE inserted >= DATE(NOW())")[0]->cnt > 0;
+		$isMaxReached = Database::query("SELECT COUNT(id) AS cnt FROM _recargas WHERE inserted >= DATE(NOW())")[0]->cnt > 0;
 
 		// do not continue if recharges max was reached today
 		if($isMaxReached) {
-			return $response->setTemplate('message.ejs', [
+			$response->setTemplate('message.ejs', [
 				"header"=>"¡Sigue intentando!",
 				"icon"=>"sentiment_very_dissatisfied",
 				"text" => "Lamentablemente, alguien más fue un poco más rápido que tú y canjeo la recarga del día. No te desanimes, mañana tendrás otra oportunidad para tratar de canjearla.",
 				"button" => ["href"=>"RECARGAS ANTERIORES", "caption"=>"Ver recargas"]]);
+			return;
 		}
 
 		// do not continue if the phone number is blocked for scams
-		$isUserBlocked = Connection::query("SELECT COUNT(*) AS cnt FROM blocked_numbers WHERE cellphone='{$buyer->cellphone}'")[0]->cnt > 0;
+		$isUserBlocked = Database::query("SELECT COUNT(*) AS cnt FROM blocked_numbers WHERE cellphone='{$buyer->cellphone}'")[0]->cnt > 0;
 
 		// check the buyer is at least one month old
 		$isNewUser = date_diff(new DateTime(), new DateTime($buyer->insertion_date))->days < 60;
 
 		// do not continue if a rule is broken
 		if($isUserBlocked || $isNewUser) {
-			return $response->setTemplate('message.ejs', [
+			$response->setTemplate('message.ejs', [
 				"header"=>"Canje rechazado",
 				"icon"=>"sentiment_very_dissatisfied",
 				"text" => "Puede que su usuario esté bloqueado o que aún no tenga permisos para comprar recargas. Por favor consulte el soporte si tiene alguna duda.",
 				"button" => ["href"=>"RECARGAS ANTERIORES", "caption"=>"Ver recargas"]]);
+			return;
 		}
 
 		// process the payment
@@ -131,21 +144,22 @@ class Service
 			MoneyNew::buy($buyer->id, $code);
 		} catch (Exception $e) {
 			echo $e->getMessage();
-			return $response->setTemplate('message.ejs', [
+			$response->setTemplate('message.ejs', [
 				"header"=>"Error inesperado",
 				"icon"=>"sentiment_very_dissatisfied",
 				"text" => "Hemos encontrado un error procesando su canje. Por favor intente nuevamente, si el problema persiste, escríbanos al soporte.",
 				"button" => ["href"=>"RECARGAS", "caption"=>"Reintentar"]]);
+			return;
 		}
 
 		// add the recharge to the table
 		// TODO: stage = 2, se mantiene mientras exista la regla de negocio "una recarga por fecha"
-		Connection::query("
+		Database::query("
 			INSERT IGNORE INTO _recargas (person_id, product_code, cellphone, stage, inserted_date)
 			VALUES ({$buyer->id}, '$code', '{$buyer->cellphone}', 2, CURRENT_DATE)");
 
 		// possitive response
-		return $response->setTemplate('message.ejs', [
+		$response->setTemplate('message.ejs', [
 			"header"=>"Canje realizado",
 			"icon"=>"sentiment_very_satisfied",
 			"text" => "Su canje se ha realizado satisfactoriamente, y su teléfono recibirá una recarga en menos de tres días. Si tiene cualquier pregunta, por favor no dude en escribirnos al soporte.",
